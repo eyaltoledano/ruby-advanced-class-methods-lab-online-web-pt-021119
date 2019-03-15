c3a6c6b71aca7eee6b83f4124929352b6d46713c
Eyal Toledano <eyaltoledano@me.com>
2019-03-15 14:51:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,9 @@ class Song
   def self.find_or_create_by_name(name)
     binding.pry
     if self.find_by_name(name) == nil
-      self.find_by_name(name)
-    else
       self.create_by_name(name)
+    else
+      self.find_by_name(name)
     end
   end
 
